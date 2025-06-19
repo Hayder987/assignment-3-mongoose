621e61aed0f204fbe4d8d16aa3554daf1d31c41e
hayder <hayderbd4290@gmail.com>
2025-06-20 00:06:20 +0600

    get book by id api route create

--- src/app/controllers/book.controller.ts
+++ src/app/controllers/book.controller.ts
@@ -27,10 +27,10 @@ bookRouter.post("/", async (req: Request, res: Response) => {
 // get all books with genre filter, sorted and limit
 bookRouter.get("/", async (req: Request, res: Response) => {
   try {
-    const { filter, sort , limit } = req.query;
+    const { filter, sort, limit } = req.query;
 
-    const query = filter ? { genre: (filter as string) } : {};
-    const sortOrder = sort as string === "asc" ? 1 : -1;
+    const query = filter ? { genre: filter as string } : {};
+    const sortOrder = (sort as string) === "asc" ? 1 : -1;
     const numberLimit = parseInt(limit as string) || 10;
 
     const data = await Book.find(query)
@@ -39,8 +39,9 @@ bookRouter.get("/", async (req: Request, res: Response) => {
 
     res.status(200).json({
       success: data.length > 0 ? true : false,
-      message: data.length > 0 ? "Books retrieved successfully" : "No book Found",
-      data : data.length > 0 ? data : [],
+      message:
+        data.length > 0 ? "Books retrieved successfully" : "No book Found",
+      data: data.length > 0 ? data : [],
     });
   } catch (error: any) {
     res.status(404).json({
@@ -51,3 +52,22 @@ bookRouter.get("/", async (req: Request, res: Response) => {
   }
 });
 
+// get book by id
+bookRouter.get("/:bookId", async (req: Request, res: Response) => {
+  try {
+    const bookId = req.params.bookId;
+    const data = await Book.findById(bookId);
+
+    res.status(200).json({
+      success: true,
+      message: "Books retrieved successfully",
+      data,
+    });
+  } catch (error: any) {
+    res.status(404).json({
+      success: false,
+      message: "Failed to retrieve books",
+      error: error.message,
+    });
+  }
+});
