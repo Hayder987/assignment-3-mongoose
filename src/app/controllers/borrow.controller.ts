dde3eba270eece7b63bd1bf4c24e21fe09351ee3
hayder <hayderbd4290@gmail.com>
2025-06-20 10:41:44 +0600

    get Borrowed Books Summary with aggregation

--- src/app/controllers/borrow.controller.ts
+++ src/app/controllers/borrow.controller.ts
@@ -13,7 +13,7 @@ borrowRouter.post("/", async (req: Request, res: Response) => {
       message: "Book borrowed successfully",
       data,
     });
-  } catch (error:any) {
+  } catch (error: any) {
     res.status(400).json({
       success: false,
       message: error.message,
@@ -22,4 +22,47 @@ borrowRouter.post("/", async (req: Request, res: Response) => {
   }
 });
 
+// get Borrowed Books Summary (Using Aggregation)
+borrowRouter.get("/", async (req: Request, res: Response) => {
+  try {
+    const summary = await Borrow.aggregate([
+      {
+        $group: {
+          _id: "$book",
+          totalQuantity: { $sum: "$quantity" },
+        },
+      },
+      {
+        $lookup: {
+          from: "books",
+          localField: "_id",
+          foreignField: "_id",
+          as: "bookInfo",
+        },
+      },
+      { $unwind: "$bookInfo" },
+      {
+        $project: {
+          _id: 0,
+          book: {
+            title: "$bookInfo.title",
+            isbn: "$bookInfo.isbn",
+          },
+          totalQuantity: 1,
+        },
+      },
+    ]);
 
+    res.json({
+      success: true,
+      message: "Borrowed books summary retrieved successfully",
+      data: summary,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Failed to retrieve summary",
+      error,
+    });
+  }
+});
